dfe986818db64d60f1e8dd4a7512eec4af475135
leprekus <leprekus@yahoo.com>
2025-10-16 12:02:05 -0700

    fix: paths

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,15 @@
 import { Button, LinkButton } from "./ui/button";
 
-const items = [ 'Home', 'Blog', 'Resume', 'Contact', 'Utils' ];
+const items = [ 
+	{ title: 'Home', path: '/' }, 
+	{ title: 'Blog', path: '/blog', 
+	{ title: 'Resume', path: '/resume' }, 
+	{ title: 'Contact', path: '/contact' }, 
+	{ title: 'Utils', path: '/utils' }
+]
 export default function Navbar() {
 	return <nav className="flex flex-wrap gap-2 md:flex-nowrap md:gap-4 min-w-full border-b-2 border-black bg-white p-4 justify-center">
-		{items.map((it, i) => <LinkButton href={it.toLowerCase()} key={`nav-button-${i}`}>{ it }</LinkButton>)}	
+		{items.map((it, i) => <LinkButton href={it.path} key={`nav-button-${i}`}>{ it.title }</LinkButton>)}	
 	</nav>
 }
 
